12af02c62e5a73ec50f3c2f8fcd79686dae35419
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-08-31 13:20:46 -0400

    jquery clone challenge

--- challenges/jquery-clone/src/index.ts
+++ challenges/jquery-clone/src/index.ts
@@ -1,12 +1,52 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-function $(selector: string): any {
-  return {} as any;
+function isHTMLElement(el: Element): el is HTMLElement {
+  return 'style' in el;
+}
+
+function $(selector: string) {
+  const element = document.querySelector(selector);
+  if (!element) {
+    return null;
+  }
+  return {
+    html(str: string) {
+      element.innerHTML = str;
+    },
+    hide() {
+      if (isHTMLElement(element)) {
+        element.style.visibility = 'hidden';
+      }
+    },
+    show() {
+      if (isHTMLElement(element)) {
+        element.style.visibility = 'visible';
+      }
+    },
+    on<K extends keyof HTMLElementEventMap>(
+      eventName: K,
+      cb: (event: HTMLElementEventMap[K]) => void
+    ) {
+      if (isHTMLElement(element)) {
+        element.addEventListener(eventName, cb);
+      }
+    },
+  };
 }
 
 namespace $ {
-  export function ajax(...args: any[]): any {
-    return {} as any;
+  export async function ajax(args: {
+    url: string;
+    success(result: object): void;
+  }) {
+    try {
+      const res = await fetch(args.url);
+      const data = await res.json();
+      if (typeof data === 'object') {
+        args.success(data);
+        return data;
+      }
+    } catch (err) {}
   }
 }
 
