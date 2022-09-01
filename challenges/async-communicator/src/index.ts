c4508d6702039b9b99b7dd935be33c06e0aec2b9
Nate Stephens <322714+ndstephens@users.noreply.github.com>
2022-09-01 16:30:22 -0400

    async challenge

--- challenges/async-communicator/src/index.ts
+++ challenges/async-communicator/src/index.ts
@@ -1,7 +1,21 @@
-import { expectType } from "tsd";
+import { expectType } from 'tsd';
 
 // IMPLEMENT THIS TYPE
-export type WrapForPenpal<T> = any;
+export type WrapForPenpal<T> = {
+  [K in keyof T]: T[K] extends { (...args: infer A): infer R }
+    ? R extends Promise<any>
+      ? T[K]
+      : (...args: A) => Promise<R>
+    : never;
+};
+
+// export type WrapForPenpal<T> = {
+//   [K in keyof T]: T[K] extends (...args: any[]) => Promise<any>
+//     ? T[K]
+//     : T[K] extends { (...args: infer A): infer R }
+//     ? (...args: A) => Promise<R>
+//     : never;
+// };
 
 /**
  * Test Scenario - Do not change anything below this line
@@ -13,15 +27,21 @@ const methods = {
   subtract(a: number, b: number): number {
     return a - b;
   },
+  asyncAdd(a: number, b: number): Promise<number> {
+    return Promise.resolve(a + b);
+  },
 };
 const asyncMethods: WrapForPenpal<typeof methods> = {} as any;
 
 let addPromise = asyncMethods.add(1, 2);
 expectType<Promise<number>>(addPromise);
 // @ts-expect-error
-expectType<typeof addPromise>("this should fail");
+expectType<typeof addPromise>('this should fail');
 
 let subtractPromise = asyncMethods.subtract(1, 2);
 expectType<Promise<number>>(subtractPromise);
 // @ts-expect-error
-expectType<typeof subtractPromise>("this should fail");
+expectType<typeof subtractPromise>('this should fail');
+
+//
+let asyncAddPromise = asyncMethods.asyncAdd(1, 2);
